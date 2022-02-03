c4eadd78a1b2dbd0744e676cb07f549054f99588
Emrys025 <c3gu@ucsd.edu>
2022-02-02 21:49:50 -0800

    skill demo pass

--- skillDemoTest.java
+++ skillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class skillDemoTest {
     @Test
     public void printNumberTest() {
-        assertEquals("The number is6666", skillDemo.printNumber(6666));
+        assertEquals("The number is 6666", skillDemo.printNumber(6666));
     }
 }
